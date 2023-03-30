cd0de3176ea28b34da491266de9d3ac0ef21f03e
Mushib Khan <ahmedmusheeb1@gmail.com>
2023-03-30 11:15:19 +0530

    Injected the user reposiotry inside the service class.

--- UserAuthentication/src/main/java/com/niit/service/UserServiceImpl.java
+++ UserAuthentication/src/main/java/com/niit/service/UserServiceImpl.java
@@ -9,10 +9,20 @@ package com.niit.service;
 import com.niit.domain.User;
 import com.niit.exception.UserAlreadyExistsException;
 import com.niit.exception.UserNotFoundException;
+import com.niit.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
+
+    private UserRepository userRepository;
+
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @Override
     public User addUser(User user) throws UserAlreadyExistsException {
         return null;
