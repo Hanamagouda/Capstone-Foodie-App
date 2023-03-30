22b6a39d7035be0fa0dcafc572b9fec9ae932a96
Mushib Khan <ahmedmusheeb1@gmail.com>
2023-03-30 12:08:43 +0530

    Written UserController and application.properties inside the UserAuthentication application.

---
+++ UserAuthentication/src/main/java/com/niit/controller/UserController.java
@@ -0,0 +1,63 @@
+/*
+ * Author : Mushib Khan
+ * Date : 30-03-2023
+ * Created with : IntelliJ IDEA Community Edition
+ */
+
+package com.niit.controller;
+
+import com.niit.domain.User;
+import com.niit.exception.UserAlreadyExistsException;
+import com.niit.exception.UserNotFoundException;
+import com.niit.security.JwtSecurityTokenGenerator;
+import com.niit.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/userAuth")
+public class UserController {
+
+    private JwtSecurityTokenGenerator jwtSecurityTokenGenerator;
+
+    private UserService userService;
+
+    @Autowired
+    public UserController(JwtSecurityTokenGenerator jwtSecurityTokenGenerator, UserService userService) {
+        this.jwtSecurityTokenGenerator = jwtSecurityTokenGenerator;
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<?> addUser(@RequestBody User user) {
+        try {
+            User registeredUser = userService.addUser(user);
+            if (registeredUser != null) {
+                return new ResponseEntity<User>(registeredUser, HttpStatus.OK);
+            } else {
+                throw new UserNotFoundException();
+            }
+        } catch (Exception exception) {
+            return new ResponseEntity<String>("Error Occurred while trying to register new user", HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> loginUser(@RequestBody User user) {
+        try {
+            User loggedUser = userService.loginUser(user);
+            if (loggedUser != null) {
+                return new ResponseEntity<>(jwtSecurityTokenGenerator.tokenGenerator(loggedUser), HttpStatus.ACCEPTED);
+            } else {
+                throw new UserAlreadyExistsException();
+            }
+        } catch (Exception exception) {
+            return new ResponseEntity<String>("Error occurred while trying to login through user", HttpStatus.BAD_REQUEST);
+        }
+    }
+}
