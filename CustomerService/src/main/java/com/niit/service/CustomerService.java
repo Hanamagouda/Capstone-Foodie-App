d7b893b2ac3200afd7c96da077390b6217f45fca
Mushib Khan <ahmedmusheeb1@gmail.com>
2023-03-31 09:44:27 +0530

    Created CustomerService interface inside the service package in CustomerService.java

---
+++ CustomerService/src/main/java/com/niit/service/CustomerService.java
@@ -0,0 +1,22 @@
+package com.niit.service;
+
+import com.niit.domain.Customer;
+import com.niit.domain.Restaurant;
+import com.niit.exception.CustomerAlreadyExistsException;
+import com.niit.exception.CustomerNotFoundException;
+import com.niit.exception.RestaurantAlreadyExistsException;
+import com.niit.exception.RestaurantNotFoundException;
+
+import java.util.List;
+
+public interface CustomerService {
+    public Customer registerCustomer(Customer customer) throws CustomerAlreadyExistsException;
+
+    public Customer addRestaurantToFavorite(String emailId, Restaurant restaurant) throws CustomerNotFoundException, RestaurantAlreadyExistsException;
+
+    public Customer getCustomerById(String emailId) throws CustomerNotFoundException;
+
+    public List<Restaurant> getFavoriteRestaurants(String emailId) throws CustomerNotFoundException;
+
+    public List<Restaurant> deleteRestaurantFromFavorite(String emailId, int restaurantId) throws RestaurantNotFoundException;
+}
