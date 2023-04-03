60b2d3f4a97ae51f59b56a000410f8aaecab02b3
Mushib Khan <ahmedmusheeb1@gmail.com>
2023-04-03 11:19:53 +0530

    Written addRestro Logic inside the RestaurantController.java

--- RestaurantService/src/main/java/com/niit/controller/RestaurantController.java
+++ RestaurantService/src/main/java/com/niit/controller/RestaurantController.java
@@ -6,6 +6,14 @@
 
 package com.niit.controller;
 
+import com.niit.domain.Restaurant;
+import com.niit.exception.RestaurantAlreadyExistsException;
+import com.niit.service.RestaurantService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,4 +21,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/restaurant")
 public class RestaurantController {
 
+    private RestaurantService restaurantService;
+
+    @Autowired
+    public RestaurantController(RestaurantService restaurantService) {
+        this.restaurantService = restaurantService;
+    }
+
+    @PostMapping("/addRestro")
+    public ResponseEntity<?> addRestaurant(@RequestBody Restaurant restaurant) {
+        try {
+            Restaurant savedRestaurant = restaurantService.addRestaurant(restaurant);
+            if (savedRestaurant == null) {
+                throw new RestaurantAlreadyExistsException();
+            } else {
+                return new ResponseEntity<Restaurant>(savedRestaurant, HttpStatus.ACCEPTED);
+            }
+        } catch (Exception exception) {
+            return new ResponseEntity<String>("Error Occurred while add new Restaurant", HttpStatus.BAD_REQUEST);
+        }
+    }
 }
