18cf6bf3395d60769b6b310a7db74e7f8a1cc3a3
Mushib Khan <ahmedmusheeb1@gmail.com>
2023-04-04 11:06:22 +0530

    Created OrderNotification.java class inside the NotificationServiceApplication with all required instance variable.

---
+++ NotificationService/src/main/java/com/niit/domain/OrderNotification.java
@@ -0,0 +1,23 @@
+/*
+ * Author : Mushib Khan
+ * Date : 04-04-2023
+ * Created with : IntelliJ IDEA Community Edition
+ */
+
+package com.niit.domain;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class OrderNotification {
+    @Id
+    private String emailId;
+    private String message;
+}
