a5e671e60d29c2b86348259f13e6240bb9ab3d81
Mushib Khan <ahmedmusheeb1@gmail.com>
2023-04-04 10:39:53 +0530

    Add Jwt filter in OrderServiceApplication.java

--- OrderService/src/main/java/com/niit/OrderServiceApplication.java
+++ OrderService/src/main/java/com/niit/OrderServiceApplication.java
@@ -1,8 +1,11 @@
 package com.niit;
 
+import com.niit.filter.JwtFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -12,4 +15,11 @@ public class OrderServiceApplication {
         SpringApplication.run(OrderServiceApplication.class, args);
     }
 
+    @Bean
+    public FilterRegistrationBean filterUrl() {
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+        filterRegistrationBean.setFilter(new JwtFilter());
+        filterRegistrationBean.addUrlPatterns("/order/user/*");
+        return filterRegistrationBean;
+    }
 }
