34e6529e79a532a59242f9fb0fd06eb8602472e2
Mushib Khan <ahmedmusheeb1@gmail.com>
2023-04-08 11:44:56 +0530

    Added one Vendor Application inside the Foodie Application

--- RestaurantService/src/main/java/com/niit/domain/Restaurant.java
+++ RestaurantService/src/main/java/com/niit/domain/Restaurant.java
@@ -25,4 +25,5 @@ public class Restaurant {
     private String restaurantLocation;
     private String restaurantImage;
     private List<Cuisine> cuisineList;
+    private String vendorId;
 }
---
+++ Vendor-Service/src/main/java/com/niit/domain/Vendor.java
@@ -0,0 +1,10 @@
+/*
+ * Author : Mushib Khan
+ * Date : 08-04-2023
+ * Created with : IntelliJ IDEA Community Edition
+ */
+
+package com.niit.domain;
+
+public class Vendor {
+}
